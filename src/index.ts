fee1284950e79ca551907f4ea3677c627bc1ae8c
Marcel Hoekstra <marcel.hoekstra@cito.nl>
2018-02-21 06:45:05 +0100

    reverted package name + added module to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { MsalConfig } from './msal-config';
 export { MsalService } from './msal.service';
 export { MsalGuard } from './msal.guard';
 export { MsalInterceptor } from './msal.interceptor';
+export { MsalModule } from './msal.module';
